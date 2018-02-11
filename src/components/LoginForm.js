a18567d71d98897f1048e6c1b464c6af09f12902
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-11 16:07:17 -0500

    Login form input fields

---
+++ src/components/LoginForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+	state = { email: '', password: '' };
+
+	render() {
+		return (
+			<Card>
+				<CardSection>
+					<Input
+						placeholder="user@gmail.com"
+						label="Email"
+						value={this.state.email}
+						onChangeText={text => this.setState({ email: text })} 
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input
+						placeholder="password"
+						label="Password"
+						value={this.state.password}
+						secureTextEntry
+						onChangeText={text => this.setState({ password: text })} 
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Log in
+					</Button>
+				</CardSection>
+			</Card>
+		)
+	}
+}
+
+export default LoginForm;
